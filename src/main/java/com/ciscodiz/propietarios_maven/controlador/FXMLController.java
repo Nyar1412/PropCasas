6ae194d53defe3f5c394bb295f8f6555c6f4e0e8
usuario <usuario@LAPTOP-3HNFMS4R>
2019-05-14 12:00:09 +0100

    v0.0intro

--- src/main/java/com/ciscodiz/propietarios_maven/controlador/FXMLController.java
+++ src/main/java/com/ciscodiz/propietarios_maven/controlador/FXMLController.java
@@ -6,19 +6,69 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javax.management.InvalidAttributeValueException;
 
 public class FXMLController implements Initializable {
     
     @FXML
-    private Label label;
-    
+    private Label lblModificar;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TextField txtIDPropietario;
+    @FXML
+    private TextField txtNombreProp;
+    @FXML
+    private TextField txtApellidoProp;
+    @FXML
+    private Button btnModificar;
+    @FXML
+    private Button btnPropAtras;
+    @FXML
+    private Button btnPropAlante;
+    @FXML
+    private Button btnPropAgregar;
+    @FXML
+    private Button btnPropEliminar;
+    @FXML
+    private Button btnPropCasasAtras;
+    @FXML
+    private Button btnPropCasasAlante;
+    @FXML
+    private Button btnPropCasasAgregar;
+    @FXML
+    private Button btnPropCasasEliminar;
+    @FXML
+    private TextField txtIdPropCasas;
+    @FXML
+    private TextField txtMetrosCasasProp;
+    @FXML
+    private Button btnPropCasasOK;
+    @FXML
+    private Button btnPropCasasCanelar;
+    @FXML
+    private Button btnPropOk;
+    @FXML
+    private Button btnPropCancelar;
+    @FXML
+    private Button btnCasasAtras;
+    @FXML
+    private Button btnCasasAdelante;
+    @FXML
+    private Button btnCasasAgregar;
+    @FXML
+    private Button btnCasasEliminar;
+    @FXML
+    private TextField txtCasasId;
+    @FXML
+    private TextField txtCasasMetros;
+    @FXML
+    private Button btnCasasOk;
+    @FXML
+    private Button btnCasasCanclar;
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
